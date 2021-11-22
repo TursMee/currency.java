77ed27b066bd345c17397745236a442740da56db
aizhanabdraiymova <abdrayimova@gmail.com>
2021-11-22 15:48:41 -0600

    sat nov 22

---
+++ src/String/StringIntro.java
@@ -0,0 +1,15 @@
+package String;
+
+public class StringIntro {
+    public static void main(String[] args) {
+        String word = "Tech";
+        String word2 = new String("Technical");
+
+        String word3 = "Tech" + "Torial";
+        System.out.println(word3);
+        word3 +=67 - 45;
+        System.out.println(word3);
+
+
+    }
+}
